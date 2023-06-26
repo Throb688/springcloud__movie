6b416e1ccec622c561fcf05b2f176bd079ce6803
Throb688 <77827171+Throb688@users.noreply.github.com>
2023-06-26 21:28:54 +0800

    Add files via upload

---
+++ hzs-gateway/src/main/java/com/example/hzsgateway/HzsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.hzsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HzsGatewayApplication.class, args);
+    }
+
+}
---
+++ hzs-gateway/src/test/java/com/example/hzsgateway/HzsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hzsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HzsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
